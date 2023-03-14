945a08cca624dfac0c1001ff64d43b3ba61afa7b
Ronja <71409721+ronjawe@users.noreply.github.com>
2023-03-14 10:00:57 +0000

    Update geom_Reihe.py
    Adding input() for q and timeout

--- Mathe/Reihen/geom_Reihe.py
+++ Mathe/Reihen/geom_Reihe.py
@@ -3,16 +3,16 @@
 #Import der mathematischen Bibliothek
 import time
 
-q = 0.5
+q = input('Welcher Wert soll für q verwendet werden?')
 k = 0.0
 s = 0.0
 
-timeout = 0.0049   # [seconds] Dauer in der 20. erreicht wird
+timeout = input('Für wie viele Sekunden soll das Skript ausgeführt werden?')  # [seconds] Dauer in der 2.0 erreicht wird: 0.0049
 
 timeout_start = time.time()
 
 while time.time() < timeout_start + timeout:
     s_temp = q**k
-    k = k + 1
-    s = s + s_temp
+    k += 1
+    s += s_temp
     print(s)
